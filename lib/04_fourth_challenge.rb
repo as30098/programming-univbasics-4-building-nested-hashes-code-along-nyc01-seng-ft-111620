e12796a1f15ab20400aedec1c367c96cfb5d3b47
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-07 13:54:20 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -11,7 +11,7 @@ def fourth_challenge
       {:name => "Mercutio",
         :age => "18",
         :attitude => "hot-headed"}
-        ]
+      ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
