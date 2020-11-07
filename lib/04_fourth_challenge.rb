f8eba20d4ed9fd75aaca5a304c40f0b083cfecb2
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-07 13:54:15 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -18,16 +18,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {
-        :name => "Steven",
+        {:name => "Steven",
         :age => "30",
-        :attitude => "confused"
-      }
-      {
-        :name => "Nurse",
+        :attitude => "confused"}
+      {:name => "Nurse",
         :age => "44",
-        :attitude => "worried"
-      }]
+        :attitude => "worried"}
+      ]
    }
 }
 
